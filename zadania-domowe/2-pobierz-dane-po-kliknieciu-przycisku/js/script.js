f53fc291e24fb028e11fd99d27802c6d758b2f99
akopowicz <anna.kopowicz@gmail.com>
2020-12-08 15:11:11 +0100

    Wykonane zadanie

---
+++ zadania-domowe/2-pobierz-dane-po-kliknieciu-przycisku/js/script.js
@@ -0,0 +1,30 @@
+let button = document.querySelector('button');
+
+const getData = () => {
+    fetch('https://akademia108.pl/api/ajax/get-post.php')
+    .then(res => res.json())
+    .then(data => {
+
+        let pId = document.createElement('p');
+        let pUserId = document.createElement('p');
+        let pTitle = document.createElement('p');
+        let pBody = document.createElement('p');
+        let hr = document.createElement('hr');
+        
+        pId.innerText = `ID: ${data.id}`;
+        pUserId.innerText = `User ID: ${data.userId}`;
+        pTitle.innerText = `Title: ${data.title}`;
+        pBody.innerText = `Body: ${data.body}`;
+
+        document.body.appendChild(pId);
+        document.body.appendChild(pUserId);
+        document.body.appendChild(pTitle);
+        document.body.appendChild(pBody);
+        document.body.appendChild(hr);
+    })
+    .catch(error => {
+        console.error(error)
+    });
+}
+
+button.addEventListener('click', getData);
