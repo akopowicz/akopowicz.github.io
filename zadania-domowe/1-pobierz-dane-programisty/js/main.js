cee64a03ddde921a685569c8533d2352f384c20f
akopowicz <anna.kopowicz@gmail.com>
2020-12-08 13:49:31 +0100

    wykonane zadanie

--- zadania-domowe/1-pobierz-dane-programisty/js/main.js
+++ zadania-domowe/1-pobierz-dane-programisty/js/main.js
@@ -1,15 +1,15 @@
 let button = document.querySelector('button');
+let div = document.createElement('div');
+div.setAttribute('id', 'dane-programisty');
+button.insertAdjacentElement('afterend', div);
 
 
 const programmerData = () => {
     fetch('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
         .then(response => response.json())
         .then((data) => {
-            let div = document.createElement('div');
-            div.setAttribute('id', 'dane-programisty');
-            button.insertAdjacentElement('afterend', div);
-
-            div.insertAdjacentHTML('beforeend', 'Imię: ' + data.imie + '<br>' + 'Nazwisko: ' + data.nazwisko + '<br>' + 'Zawód: ' + data.zawod + '<br>' + 'Firma: ' + data.firma);
+          
+            div.insertAdjacentHTML('beforeend', 'Imię: ' + data.imie + '<br>' + 'Nazwisko: ' + data.nazwisko + '<br>' + 'Zawód: ' + data.zawod + '<br>' + 'Firma: ' + data.firma + '<br>');
         });
 };
 
