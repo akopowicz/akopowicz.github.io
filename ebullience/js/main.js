6def5cc4280a7d62ba7de677bf688dfd34d3cab7
akopowicz <anna.kopowicz@gmail.com>
2021-04-12 21:50:11 +0200

    zrobione

--- ebullience/js/main.js
+++ ebullience/js/main.js
@@ -4,18 +4,36 @@ var hamburger = document.querySelector('.hamburger');
 let show = false;
 let footer = document.querySelector('footer');
 let mainImage = document.querySelector('.main-img');
+let inscription = document.querySelector('.inscription');
+let contactInformations = document.querySelector('.contact-informations')
+let form = document.querySelector('form');
 
-setTimeout(() => {
-    if (window.innerWidth > 1000) {
-        mainImage.src = "img/pexels-harsch-shivam-20076474.png";
+if (document.body.id == "home") {
+
+    setTimeout(() => {
+        if (window.innerWidth > 1000) {
+            mainImage.src = "img/pexels-harsch-shivam-20076474.png";
+            inscription.style.left = navList.offsetLeft + 20 + "px";
+        }
+    }, 10);
+
+
+    let header = document.querySelector('header');
+    setTimeout(() => {
+        header.classList.add('header-animation');
+    }, 500);
+}
+if (window.innerWidth > 1000) {
+    if (document.body.id == "help") {
+        form.style.left = navList.offsetLeft + 20 + "px";
     }
-}, 10);
+
+    if (document.body.id == "contact") {
+        contactInformations.style.left = navList.offsetLeft + 20 + "px";
+    }
+}
 
 
-let header = document.querySelector('header');
-setTimeout(() => {
-    header.classList.add('header-animation');
-}, 500);
 
 hamburger.addEventListener('click', () => {
     if (!show) {
