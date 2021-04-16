20549f4fe7c71785784bdc1f7ff532f696c84fbc
akopowicz <anna.kopowicz@gmail.com>
2021-04-16 23:41:58 +0200

    final version

--- ebullience1/js/main.js
+++ ebullience1/js/main.js
@@ -4,24 +4,38 @@ var hamburger = document.querySelector('.hamburger');
 let show = false;
 let footer = document.querySelector('footer');
 let mainImage = document.querySelector('.main-img');
-let inscription = document.querySelector('.inscription');
+let homeInformation = document.querySelector('.home-information');
 let contactInformations = document.querySelector('.contact-informations')
 let form = document.querySelector('form');
 
 if (document.body.id == "home") {
 
-    setTimeout(() => {
+    window.onload = () => {
         if (window.innerWidth > 1000) {
             mainImage.src = "img/pexels-harsch-shivam-20076474.png";
-            inscription.style.left = navList.offsetLeft + 20 + "px";
+            homeInformation.style.left = navList.offsetLeft + 20 + "px";
         }
-    }, 10);
-
+    }
 
     let header = document.querySelector('header');
     setTimeout(() => {
         header.classList.add('header-animation');
     }, 500);
+}
+
+if (document.body.id == "about") {
+    let inscriptionAbout = document.querySelector('.about-information')
+    let aboutHeader = document.querySelector('.about-header')
+    window.onload = () => {
+        if (window.innerWidth > 700) {
+            mainImage.src = "img/pexels-wangming_photo-3549414.png";
+        }
+
+        if (inscriptionAbout.clientHeight > mainImage.clientHeight) {
+            inscriptionAbout.style.overflow = "auto"
+            console.log(inscriptionAbout.offsetHeight)
+        }
+    }
 }
 if (window.innerWidth > 1000) {
     if (document.body.id == "help") {
@@ -32,8 +46,6 @@ if (window.innerWidth > 1000) {
         contactInformations.style.left = navList.offsetLeft + 20 + "px";
     }
 }
-
-
 
 hamburger.addEventListener('click', () => {
     if (!show) {
